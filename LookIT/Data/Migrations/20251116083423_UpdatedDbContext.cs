5d79025e2318d5e3fc204cc53106c7b1b5681b1f
Negru Georgiana-Raluca <raluca.negru05@gmail.com>
2025-11-16 10:39:50 +0200

    Updated ApplicationDbContext

---
+++ LookIT/Data/Migrations/20251116083423_UpdatedDbContext.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LookIT.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupMember_AspNetUsers_MemberId",
+                table: "GroupMember");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupMember_Groups_GroupId",
+                table: "GroupMember");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GroupMember",
+                table: "GroupMember");
+
+            migrationBuilder.RenameTable(
+                name: "GroupMember",
+                newName: "GroupMembers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_GroupMember_MemberId",
+                table: "GroupMembers",
+                newName: "IX_GroupMembers_MemberId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GroupMembers",
+                table: "GroupMembers",
+                columns: new[] { "GroupId", "MemberId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupMembers_AspNetUsers_MemberId",
+                table: "GroupMembers",
+                column: "MemberId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupMembers_Groups_GroupId",
+                table: "GroupMembers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupMembers_AspNetUsers_MemberId",
+                table: "GroupMembers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupMembers_Groups_GroupId",
+                table: "GroupMembers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GroupMembers",
+                table: "GroupMembers");
+
+            migrationBuilder.RenameTable(
+                name: "GroupMembers",
+                newName: "GroupMember");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_GroupMembers_MemberId",
+                table: "GroupMember",
+                newName: "IX_GroupMember_MemberId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GroupMember",
+                table: "GroupMember",
+                columns: new[] { "GroupId", "MemberId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupMember_AspNetUsers_MemberId",
+                table: "GroupMember",
+                column: "MemberId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupMember_Groups_GroupId",
+                table: "GroupMember",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
