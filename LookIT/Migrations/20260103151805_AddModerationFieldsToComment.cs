1596cdb7c56e9883e82eb038700bfde1af39fb47
Negru Georgiana-Raluca <raluca.negru05@gmail.com>
2026-01-03 18:00:03 +0200

    Added AI Companion for detecting inappropriate language in adding and editing a Comment

---
+++ LookIT/Migrations/20260103151805_AddModerationFieldsToComment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LookIT.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModerationFieldsToComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FlagCategory",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFlagged",
+                table: "Comments",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModeratedAt",
+                table: "Comments",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlagCategory",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "IsFlagged",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "ModeratedAt",
+                table: "Comments");
+        }
+    }
+}
